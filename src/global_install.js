08e08c116090c3e737aad7809d65209636eda235
Plasmatium <slv309@163.com>
2017-02-20 23:02:09 +0800

    Fixed some bugs, but ph_position is an error

--- src/global_install.js
+++ src/global_install.js
@@ -1,4 +1,4 @@
-console.log("Installing global_install.js.")
+console.log("Installing global_install.js.");
 
 global.x = 'x';
 
@@ -12,13 +12,13 @@ const _curry1 = function(func, arg_num_left, arg_list) {
     let new_arg_list = [...arg_list, ...arguments];
     let diff_arg_num = arg_num_left - arguments.length;
     let new_arg_num_left = diff_arg_num >= 0 ? diff_arg_num : 0;
-    return _curry(func, new_arg_num_left, new_arg_list);
+    return _curry1(func, new_arg_num_left, new_arg_list);
   }
-}
+};
 
 global.curry1 = (func) => {
-  return _curry(func, func.length, []);
-}
+  return _curry1(func, func.length, []);
+};
 
 //==========curry with place_holder==========
 const __ = Symbol('@@place_holder');
@@ -37,33 +37,34 @@ const _curry = function(func, arg_list, curr_args, ph_position) {
   // arg_list：当前已经收集的真实参数列表
   // curr_args：当前的调用所传递进来的参数列表
   // ph_position：placeholder所在位置
-
-  if(ph_position.length === 0) {
-    return func(...arg_list);
-  }
-
-  //
-  for(let i=0; i<Math.min(ph_position.length, curr_args.length); i++) {
+debugger;
+  let len1 = ph_position.length;
+  let len2 = curr_args.length;
+  for(let i=0; i<Math.min(len1, len2); i++) {
     let arg = curr_args.shift();
     if(arg !== __) {
       let idx = ph_position.shift();
-      arg_list[idx] = curr_args.shift();
+      arg_list[idx] = arg;
     }
   }
 
-  return function () {
-    _curry(func, [...arg_list], arguments, [...ph_position]);
+  if(ph_position.length === 0) {
+      return func(...arg_list);
   }
-}
+
+  return function () {
+    return _curry(func, [...arg_list], [...arguments], [...ph_position]);
+  }
+};
 
 global.curry = (func) => {
   let arg_list = new Array(func.length);
   arg_list.fill(__);
   let ph_position = arg_list.map((val, idx) => idx);
   return function () {
-    _curry(func, arg_list, arguments, arguments, ph_position);
+    return _curry(func, [...arg_list], [...arguments], [...ph_position]);
   }
-}
+};
 
 
 //test func
